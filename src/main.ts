f03b5524f6b8ab02c60b8ae48f8a591bdebeeb5e
cecep31 <cecepjanuardi31@gmail.com>
2024-04-28 15:25:50 +0700

    middel ware

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { Hono } from 'hono'
-import { Database } from './config/database'
+import { logger } from "hono/logger"
+import { compress } from 'hono/compress'
 import UserController from './controllers/userController'
 
 const app = new Hono()
 
+app.use(logger())
+app.use(compress())
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-const database = new Database();
-
-
 app.route('/users', UserController)
 
-export default app
+export default {
+  port: 3001,
+  fetch: app.fetch
+}
+
