43ffedefb81dfa73391f39ea31cc6100b8f930ef
cecep31 <cecepjanuardi31@gmail.com>
2024-05-01 20:44:03 +0700

    improve test

--- src/test/authservice.test.ts
+++ src/test/authservice.test.ts
@@ -1,12 +1,24 @@
-import { describe, it, expect } from 'bun:test'
-import AuthService from '../pkg/services/authService'
-import { PrismaClient } from '@prisma/client'
+import { describe, it, expect, beforeEach, afterEach } from 'bun:test';
+import AuthService from '../pkg/services/authService';
+import { PrismaClient } from '@prisma/client';
 
-describe('authservice', () => {
-    it('signin', async () => {
-        const database = new PrismaClient()
-        const authService = new AuthService(database)
-        const token = await authService.signIn('guest@pilput.dev', 'guestguest')
-        expect(token).toEqual({ access_token: expect.any(String) });
-    })
-})
+describe('AuthService', () => {
+    const testEmail = 'guest@pilput.dev';
+    const testPassword = 'guestguest';
+    let db: PrismaClient;
+    let authService: AuthService;
+
+    beforeEach(() => {
+        db = new PrismaClient();
+        authService = new AuthService(db);
+    });
+
+    afterEach(async () => {
+        await db.$disconnect();
+    });
+
+    it('signs in a user with valid credentials', async () => {
+        const token = await authService.signIn(testEmail, testPassword);
+        expect(token).toHaveProperty('access_token');
+    });
+});
--- src/test/userservice.test.ts
+++ src/test/userservice.test.ts
@@ -1,12 +1,21 @@
 
-import { describe, it, expect,  } from 'bun:test'
+import { describe, it, expect, beforeEach, afterEach  } from 'bun:test'
 import UserService from '../pkg/services/userServices'
 import { PrismaClient } from '@prisma/client'
 
 describe('userservice', () => {
+    let db: PrismaClient;
+    let userservice: UserService;
+
+    beforeEach(() => {
+        db = new PrismaClient();
+        userservice = new UserService(db);
+    });
+
+    afterEach(async () => {
+        await db.$disconnect();
+    });
     it('getUsers', async () => {
-        const database = new PrismaClient()
-        const userservice = new UserService(database)
         const users = await userservice.getUsers()
         expect(users.length).toBeGreaterThanOrEqual(1)
     })
