bcfc779bf7b22234bc98d351ae5b60d80b19985f
cecep31 <cecepjanuardi31@gmail.com>
2024-06-06 17:10:10 +0700

    fix memory leak

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,23 +2,22 @@ import { Hono } from "hono";
 import AuthService from "../pkg/services/authService";
 import { z } from "zod";
 import { zValidator } from '@hono/zod-validator'
-import {db} from '../database/drizzel'
+import { db } from '../database/drizzel'
 
-export default function setupAuth() {
-    const authController = new Hono();
-    
-    authController.post("/login", zValidator(
-        'json',
-        z.object({
-            email: z.string().email(),
-            password: z.string()
-        })
-    ), async (c) => {
-        const authService = new AuthService(db);
-        const body = await c.req.json();
-        const { email, password } = body;
-        const token = await authService.signIn(email, password);
-        return c.json(token);
+const authController = new Hono();
+
+authController.post("/login", zValidator(
+    'json',
+    z.object({
+        email: z.string().email(),
+        password: z.string()
     })
-    return authController
-}
+), async (c) => {
+    const authService = new AuthService(db);
+    const body = await c.req.json();
+    const { email, password } = body;
+    const token = await authService.signIn(email, password);
+    return c.json(token);
+})
+export default authController
+
