1b0a03c4af025237b821c3b012a5ff3b203fbef3
cecep31 <cecepjanuardi31@gmail.com>
2024-06-07 22:51:58 +0700

    increase connection

--- src/database/drizzel.ts
+++ src/database/drizzel.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '../schema/schema';
 
-const queryClient = postgres(process.env.DATABASE_URL!);
+const queryClient = postgres(process.env.DATABASE_URL!, { max: 20 });
 
 export const db = drizzle(queryClient, { schema: schema, logger: process.env.NODE_ENV === 'development' ? true : false });
 
