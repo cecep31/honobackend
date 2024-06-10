f0e5368e697e1395cffeb8383c3740eaa64fbe31
cecep31 <cecepjanuardi31@gmail.com>
2024-06-10 20:50:39 +0700

    logger

--- src/database/drizzel.ts
+++ src/database/drizzel.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '../schema/schema';
 
-const queryClient = postgres(process.env.DATABASE_URL!, { max: 20 });
+const queryClient = postgres(process.env.DATABASE_URL!, { max: 15 });
 
-export const db = drizzle(queryClient, { schema: schema, logger: process.env.NODE_ENV === 'development' ? true : false });
+export const db = drizzle(queryClient, { schema: schema, logger: process.env.NODE_ENV === 'production' ? false : true });
 
