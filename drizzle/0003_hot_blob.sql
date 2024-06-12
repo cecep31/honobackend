f24fe1c6ee4d0b61b317821b326f1e3e85ae0a03
cecep31 <cecepjanuardi31@gmail.com>
2024-06-12 21:34:56 +0700

    update some structure table and improve code

---
+++ drizzle/0003_hot_blob.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "posts" ALTER COLUMN "title" SET DATA TYPE varchar;--> statement-breakpoint
+ALTER TABLE "posts" ALTER COLUMN "slug" SET DATA TYPE varchar;
---
+++ drizzle/0004_cool_rictor.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "posts" ALTER COLUMN "title" SET DATA TYPE varchar(255);--> statement-breakpoint
+ALTER TABLE "posts" ALTER COLUMN "slug" SET DATA TYPE varchar(255);--> statement-breakpoint
+ALTER TABLE "tags" ALTER COLUMN "name" SET DATA TYPE varchar(30);
