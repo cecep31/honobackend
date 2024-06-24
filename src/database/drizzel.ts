11e36638a39883b433a28b8133facb0256f4421d
cecep31 <cecepjanuardi31@gmail.com>
2024-06-24 23:55:05 +0700

    max connection

--- src/database/drizzel.ts
+++ src/database/drizzel.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema/schema';
 
-const queryClient = postgres(process.env.DATABASE_URL!, { max: 15 });
+const queryClient = postgres(process.env.DATABASE_URL!, { max: parseInt(process.env.MAX_CONNECTION || '20') });
 
 export const db = drizzle(queryClient, { schema: schema, logger: process.env.NODE_ENV === 'production' ? undefined : true });
 
