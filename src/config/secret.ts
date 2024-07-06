f2f6ac06ecb7b22837f7bffb235cf0a27c260d62
cecep31 <cecepjanuardi31@gmail.com>
2024-07-06 23:04:31 +0700

    update dependency

--- src/config/secret.ts
+++ src/config/secret.ts
@@ -1,4 +1,3 @@
-
 export const getSecret = {
     jwtsecret: process.env.JWT_KEY!
 }
--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -34,8 +34,6 @@ export const postController = new Hono()
     return c.json(posts);
   })
   .get("/tag/:tag", async (c) => {
-    // const limit = parseInt(c.req.query('limit')!) || 100
-    // const offset = parseInt(c.req.query('offset')!) || 0
     const posts = await PostService.getPostsByTag(c.req.param("tag"));
     return c.json(posts);
   })
