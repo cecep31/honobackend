d221d80bfa4faf63a7df23d905eda132869211fb
cecep31 <cecepjanuardi31@gmail.com>
2024-07-11 08:58:54 +0700

    add pilput logger

---
+++ src/middlewares/logger.ts
@@ -0,0 +1,31 @@
+import { createMiddleware } from "hono/factory";
+
+const colors = {
+    reset: "\x1b[0m",
+    blue: "\x1b[34m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    red: "\x1b[31m",
+};
+
+export const pilputLogger = createMiddleware(async (c, next) => {
+  const start = Date.now();
+  await next();
+  const end = Date.now();
+  const method = c.req.method;
+  const url = c.req.url;
+  const duration = end - start;
+  const status = c.res.status;
+  let tatuscolor = "";
+  if (status >= 200 && status < 300) {
+    tatuscolor = colors.green;
+  }else if (status >= 300 && status < 400) {
+    tatuscolor = colors.yellow;
+  }else if (status >= 400 && status < 500) {
+    tatuscolor = colors.blue;
+  }else if (status >= 500 && status < 600) {
+    tatuscolor = colors.red;
+  }
+
+  console.log(`[${new Date().toISOString()}] ${colors.blue}${method}${colors.reset} ${colors.green}${url}${colors.reset} - ${colors.yellow}${duration}ms${colors.reset} - ${tatuscolor}${status}${colors.reset}`);
+});
--- src/server/app.ts
+++ src/server/app.ts
@@ -1,17 +1,28 @@
-import { Hono } from "hono"
-import { cors } from "hono/cors"
-import { HTTPException } from "hono/http-exception"
-import { logger } from "hono/logger"
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
+import { pilputLogger } from "../middlewares/logger";
 
 export const app = new Hono()
-    .use(logger())
-    .use(cors({ origin: ["http://localhost:3000", "http://localhost:5173", "https://pilput.dev", "https://app.pilput.dev", "https://dash.pilput.dev"], }))
-    .get('/', async (c) => {
-        return c.json({ "message": "hello world" })
-    }).onError((err, c) => {
-        if (err instanceof HTTPException) {
-            return c.json({ "message": err.message }, err.status)
-        }
-        console.log(err);
-        return c.json({ "message": "internal server error" }, 500)
+  .use(pilputLogger)
+  .use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://pilput.dev",
+        "https://app.pilput.dev",
+        "https://dash.pilput.dev",
+      ],
     })
+  )
+  .get("/", async (c) => {
+    return c.json({ message: "hello world" });
+  })
+  .onError((err, c) => {
+    if (err instanceof HTTPException) {
+      return c.json({ message: err.message }, err.status);
+    }
+    console.log(err);
+    return c.json({ message: "internal server error" }, 500);
+  });
