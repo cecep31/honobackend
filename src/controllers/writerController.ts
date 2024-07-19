e33d4842a4a50b3e9a24689e75006299dd888dd2
cecep31 <cecepjanuardi31@gmail.com>
2024-07-19 17:03:58 +0700

    update for error response

--- src/controllers/writerController.ts
+++ src/controllers/writerController.ts
@@ -1,15 +1,11 @@
-import { Hono } from "hono"
+import { Hono } from "hono";
 import { WritetService } from "../pkg/services/writerService";
 
-export const writerController = new Hono()
-    .get("/:username", async (c) => {
-        const username = c.req.param("username");
-        console.log(username);
-        
-        const user = await WritetService.getWriterByUsername(username);
-        if (!user) {
-            return c.text("user not found", 404);
-        }
-        return c.json(user);
-    })
-
+export const writerController = new Hono().get("/:username", async (c) => {
+  const username = c.req.param("username");
+  const user = await WritetService.getWriterByUsername(username);
+  if (!user) {
+    return c.json({ message: "user not found" }, 404);
+  }
+  return c.json(user);
+});
