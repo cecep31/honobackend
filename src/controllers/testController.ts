f160145dc5ff2f5c525415a321ec292f83efb689
cecep31 <cecepjanuardi31@gmail.com>
2024-07-19 14:36:21 +0700

    move the variable location

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,38 +1,35 @@
-import { Hono } from "hono"
-import {faker} from '@faker-js/faker'
+import { Hono } from "hono";
+import { faker } from "@faker-js/faker";
 
 type faketype = {
-    id: number
-    name: string
-    age: number
-    posts: [
-        {
-            id: number
-            title: string
-            body?: string
-        }
-    ]
-}
+  id: number;
+  name: string;
+  age: number;
+  posts: [
+    {
+      id: number;
+      title: string;
+      body?: string;
+    }
+  ];
+};
 
-const fakedata: faketype[] = []
+export const testController = new Hono().get("/", async (c) => {
+  const fakedata: faketype[] = [];
 
-for (let index = 0; index < 1000; index++) {
+  for (let index = 0; index < 1000; index++) {
     fakedata.push({
-        id: index,
-        name: faker.person.fullName(),
-        age: faker.number.int({ min: 1, max: 100 }),
-        posts: [
-            {
-                id: index,
-                title: faker.lorem.sentence(),
-                body: faker.lorem.paragraphs()
-            }
-        ]
-    })
-    
-}
-
-export const testController = new Hono()
-    .get("/", async (c) => {
-        return c.json(fakedata)
-    })
+      id: index,
+      name: faker.person.fullName(),
+      age: faker.number.int({ min: 1, max: 100 }),
+      posts: [
+        {
+          id: index,
+          title: faker.lorem.sentence(),
+          body: faker.lorem.paragraphs(),
+        },
+      ],
+    });
+  }
+  return c.json(fakedata);
+});
