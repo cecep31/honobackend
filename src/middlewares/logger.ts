bb72eda8e27e0daa4680bacd0285093bd3563a14
cecep31 <cecepjanuardi31@gmail.com>
2024-07-26 15:33:08 +0700

    adjustmend

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -30,11 +30,6 @@ export const pilputLogger = createMiddleware(async (c, next) => {
   // {"level":"info","ip":"127.0.0.1","latency":"1.8629287s","status":200,"method":"GET","url":"/api/v2/posts","time":"2024-07-26T15:23:15+07:00","message":"Success"}
 
   console.log(
-    `{"method":"${colors.blue}${method}${colors.reset}",
-    "url":"${colors.green}${url}${colors.reset}",
-     "latency":"${colors.yellow}${duration}ms${colors.reset}",
-     "status":"${tatuscolor}${status}${colors.reset}",
-     "requestId":"${colors.blue}${c.get("requestId")}${colors.reset}",
-     "userAgent":"${c.req.header("user-agent")}"}`
+    `{"method":"${colors.blue}${method}${colors.reset}","url":"${colors.green}${url}${colors.reset}","latency":"${colors.yellow}${duration}ms${colors.reset}","status":"${tatuscolor}${status}${colors.reset}","requestId":"${colors.blue}${c.get("requestId")}${colors.reset}","userAgent":"${c.req.header("user-agent")}"}`
   );
 });
