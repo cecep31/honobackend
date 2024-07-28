768e4bade9c912ba287e2fac31d049b8f3022ca4
cecep31 <cecepjanuardi31@gmail.com>
2024-07-28 14:14:38 +0700

    endpoint to get me

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,40 +1,51 @@
-import { Hono } from "hono"
+import { Hono } from "hono";
 import { UserService } from "../pkg/services/userServices";
-import { validate as validateUuid } from 'uuid';
-import { auth } from '../middlewares/auth'
+import { validate as validateUuid } from "uuid";
+import { auth } from "../middlewares/auth";
 import { superAdmin } from "../middlewares/superAdmin";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 
 export const userController = new Hono()
-    .get("/", auth, async (c) => {
-        return c.json(await UserService.getUsers());
-    })
-    .get("/:id", auth, async (c) => {
-        const id = c.req.param("id");
-        //check id is uuid
-        if (!validateUuid(id)) {
-            return c.text("invalid id", 400);
-        }
-        const user = await UserService.gerUser(id);
-        if (!user) {
-            return c.text("user not found", 404);
-        }
-        return c.json(user);
-    })
-    .post("/", auth, superAdmin, zValidator('json',
-        z.object({
-            first_name: z.string(),
-            last_name: z.string(),
-            email: z.string(),
-            password: z.string(),
-        })), async (c) => {
-            const body = await c.req.json() as PostUser;
-            return c.json(await UserService.addUser(body))
-        })
-    .delete("/:id", auth, superAdmin, async (c) => {
-        const id = c.req.param('id')
-        const user = await UserService.deleteUser(id)
-        return c.json(user)
-    })
-
+  .get("/", auth, async (c) => {
+    return c.json(await UserService.getUsers());
+  })
+  .get("/me", auth, async (c) => {
+    const user = c.get("jwtPayload") as jwtPayload;
+    return await c.json(await UserService.gerUserMe(user.id));
+  })
+  .get("/:id", auth, async (c) => {
+    const id = c.req.param("id");
+    //check id is uuid
+    if (!validateUuid(id)) {
+      return c.text("invalid id", 400);
+    }
+    const user = await UserService.gerUser(id);
+    if (!user) {
+      return c.text("user not found", 404);
+    }
+    return c.json(user);
+  })
+  .post(
+    "/",
+    auth,
+    superAdmin,
+    zValidator(
+      "json",
+      z.object({
+        first_name: z.string(),
+        last_name: z.string(),
+        email: z.string(),
+        password: z.string(),
+      })
+    ),
+    async (c) => {
+      const body = (await c.req.json()) as PostUser;
+      return c.json(await UserService.addUser(body));
+    }
+  )
+  .delete("/:id", auth, superAdmin, async (c) => {
+    const id = c.req.param("id");
+    const user = await UserService.deleteUser(id);
+    return c.json(user);
+  });
--- src/pkg/services/userServices.ts
+++ src/pkg/services/userServices.ts
@@ -1,48 +1,67 @@
-import { desc, eq, isNull } from 'drizzle-orm'
-import { profiles, users } from '../../database/schema/schema';
-import { HTTPException } from 'hono/http-exception';
-import { db } from '../../database/drizzel';
+import { desc, eq, isNull } from "drizzle-orm";
+import { profiles, users } from "../../database/schema/schema";
+import { HTTPException } from "hono/http-exception";
+import { db } from "../../database/drizzel";
 
 export class UserService {
+  static getUsers() {
+    return db.query.users.findMany({
+      columns: {
+        password: false,
+      },
+      orderBy: [desc(users.created_at)],
+      where: isNull(users.deleted_at),
+    });
+  }
+  static gerUser(id: string) {
+    return db.query.users.findFirst({
+      columns: { password: false },
+      where: eq(users.id, id),
+    });
+  }
+  static gerUserMe(id: string) {
+    return db.query.users.findFirst({
+      columns: { password: false },
+      with: { profile: true },
+      where: eq(users.id, id),
+    });
+  }
 
-    static getUsers() {
-        return db.query.users.findMany({
-            columns: {
-                password: false
-            },
-            orderBy: [desc(users.created_at)],
-            where: isNull(users.deleted_at),
-        })
+  static async deleteUser(user_id: string) {
+    const look = await this.gerUser(user_id);
+    if (!look) {
+      throw new HTTPException(404, { message: "User not found" });
     }
-    static gerUser(id: string) {
-        return db.query.users.findFirst({ columns: { password: false }, where: eq(users.id, id) })
-    }
+    return await db
+      .update(users)
+      .set({ deleted_at: new Date().toISOString() })
+      .where(eq(users.id, user_id))
+      .returning({ id: users.id });
+  }
+  static async addUser(body: PostUser) {
+    const hash_password = Bun.password.hashSync(body.password, {
+      algorithm: "bcrypt",
+      cost: 12,
+    });
+    const resultuser = await db
+      .insert(users)
+      .values({
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        password: hash_password,
+        image: body.image,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      })
+      .returning({ id: users.id });
 
-    static async deleteUser(user_id: string) {
-        const look = await this.gerUser(user_id)
-        if (!look) {
-            throw new HTTPException(404, { message: "User not found" })
-        }
-        return await db.update(users).set({ deleted_at: new Date().toISOString() }).where(eq(users.id, user_id)).returning({ id: users.id });
-    }
-    static async addUser(body: PostUser) {
-        const hash_password = Bun.password.hashSync(body.password, { algorithm: 'bcrypt', cost: 12 })
-        const resultuser = await db.insert(users).values({
-            first_name: body.first_name,
-            last_name: body.last_name,
-            email: body.email,
-            password: hash_password,
-            image: body.image,
-            created_at: new Date().toISOString(),
-            updated_at: new Date().toISOString(),
-        }).returning({ id: users.id })
+    await db.insert(profiles).values({
+      user_id: resultuser[0].id,
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    });
 
-        await db.insert(profiles).values({
-            user_id: resultuser[0].id,
-            created_at: new Date().toISOString(),
-            updated_at: new Date().toISOString(),
-        })
-
-        return resultuser
-    }
+    return resultuser;
+  }
 }
