57fa51de538ae6733a9f34e9964fece0d6c1842d
cecep31 <cecepjanuardi31@gmail.com>
2024-08-12 21:30:41 +0700

    update for handle robot

--- src/pkg/services/writerService.ts
+++ src/pkg/services/writerService.ts
@@ -1,12 +1,19 @@
+import { HTTPException } from "hono/http-exception";
 import { UserRepository } from "../repository/userRepository";
 
 export class WritetService {
-    private userrepository: UserRepository;
-    constructor(){
-        this.userrepository = new UserRepository()
-    } 
-    async getWriterByUsername(username: string) {
-        const user = await this.userrepository.getUserByUsernameProfile(username);
-        return user
+  private userrepository: UserRepository;
+  constructor() {
+    this.userrepository = new UserRepository();
+  }
+  async getWriterByUsername(username: string) {
+    const usernameRegex = /^[a-zA-Z0-9_]{3,16}$/;
+    function validateUsername(username: string) {
+      return usernameRegex.test(username);
     }
+    if (!validateUsername(username)) {
+      throw new HTTPException(404, { message: "Username not valid" });
+    }
+    return await this.userrepository.getUserByUsernameProfile(username);
+  }
 }
