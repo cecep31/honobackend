405d701f348fe4562e2aa76cccdf123da06cf4e9
cecep31 <cecepjanuardi31@gmail.com>
2024-09-09 20:20:11 +0700

    forget little something

--- src/config/github.ts
+++ src/config/github.ts
@@ -1,5 +1,5 @@
 export const credential = {
   CLIENT_ID: process.env["GITHUB_CLIENT_ID"],
   CLIENT_SECRET: process.env["GITHUB_CLIENT_SECRET"],
-  REDIRECT_URI : process.env["GITHUB_REDIRECT_URI"] || "https://hono.pilput.dev/auth/github/callback"
+  REDIRECT_URI : process.env["GITHUB_REDIRECT_URI"] || "https://hono.pilput.dev/auth/oauth/github/callback"
 };
