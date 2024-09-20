1668837901905f25c73ef2c051dae9d49576389a
cecep31 <cecepjanuardi31@gmail.com>
2024-09-20 17:21:37 +0700

    300 request per minutes

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import { timeout } from "hono/timeout";
+import { rateLimiter } from "hono-rate-limiter";
 
 export function setupMiddlewares(app: Hono) {
   app.use(logger());
@@ -16,5 +17,11 @@ export function setupMiddlewares(app: Hono) {
       ],
     })
   );
+  app.use(rateLimiter({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    limit: 300, // Limit each IP to 300 requests per `window` (here, per 1 minute).
+    standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    keyGenerator: (_c) => "<unique_key>", // Method to generate custom identifiers for clients.
+  }));
   app.use(timeout(30000));
 }
