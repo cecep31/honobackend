d3db33abfb1e56c0326580683860ad20cac10772
cecep31 <cecepjanuardi31@gmail.com>
2024-09-21 15:03:08 +0700

    super admin middle ware fix

--- src/middlewares/superAdmin.ts
+++ src/middlewares/superAdmin.ts
@@ -1,10 +1,13 @@
 import { createMiddleware } from "hono/factory";
 import { HTTPException } from "hono/http-exception";
 import type { jwtPayload } from "../types/auth";
+import { UserRepository } from "../pkg/repository/userRepository";
 
 export const superAdmin = createMiddleware(async (c, next) => {
   const authorization = c.get("jwtPayload") as jwtPayload;
-  if (authorization.issuperadmin) {
+  const userRepo = new UserRepository();
+  const user = await userRepo.getUser(authorization.id);
+  if (user?.issuperadmin) {
     await next();
   } else {
     throw new HTTPException(403, { message: "forbidden" });
