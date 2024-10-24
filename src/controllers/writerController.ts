997106c0d80d733e6585684f3b3402acd567e56d
cecep31 <cecepjanuardi31@gmail.com>
2024-10-24 22:50:15 +0700

    good

--- src/controllers/writerController.ts
+++ src/controllers/writerController.ts
@@ -1,10 +1,10 @@
 import { Hono } from "hono";
-import { writetService, postService } from "../pkg/service";
+import { writerService, postService } from "../pkg/service";
 
 export const writerController = new Hono()
   .get("/:username", async (c) => {
     const username = c.req.param("username");
-    const user = await writetService.getWriterByUsername(username);
+    const user = await writerService.getWriterByUsername(username);
     if (!user) {
       return c.json({ message: "user not found" }, 404);
     }
--- src/pkg/service.ts
+++ src/pkg/service.ts
@@ -14,4 +14,4 @@ export const postService = new PostService(postrepository, tagrepository);
 export const authService = new AuthService(userrepository, sessionRepository);
 export const userService = new UserService(userrepository);
 export const tagService = new TagService(tagrepository);
-export const writetService = new WritetService(userrepository);
+export const writerService = new WritetService(userrepository);
