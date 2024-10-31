07c076069c96dd4de570855cee2c806134fb6ff2
cecep31 <cecepjanuardi31@gmail.com>
2024-10-31 18:24:19 +0700

    enhance database connection settings with timeout and max connections

--- src/database/drizzel.ts
+++ src/database/drizzel.ts
@@ -1,11 +1,14 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema/schema";
 
 export const db = drizzle({
   connection: {
     url: process.env["DATABASE_URL"],
     ssl: true,
+    idle_timeout: 30000,
+    connect_timeout: 2000,
+    max: 10,
   },
   logger: process.env["SQL_LOG"] ? true : undefined,
-  schema: schema
+  schema: schema,
 });
