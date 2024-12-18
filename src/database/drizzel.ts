8015d89c267988137ab1a955bbf9b1b02c440c2a
cecep31 <cecepjanuardi@outlokk.com>
2024-12-18 14:22:41 +0700

    turn back to postgresjs

--- src/database/drizzel.ts
+++ src/database/drizzel.ts
@@ -1,19 +1,13 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { drizzle as drizzle_neon } from "drizzle-orm/neon-serverless";
 import * as schema from "./schemas/postgre/schema";
 import { getSecret } from "../config/secret";
 
-export const db_old = drizzle({
+export const db = drizzle({
   connection: {
     url: getSecret.db_url,
     ssl: "prefer",
     max: 10,
   },
-  schema: schema,
-});
-
-export const db = drizzle_neon({
-  connection: getSecret.db_url,
   schema: schema,
 });
 
