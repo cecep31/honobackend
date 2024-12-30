8702263bedc744a4cb1af97f17e2c5b6ebfa70c2
cecep <cecepjanuardi@outlook.com>
2024-12-30 09:54:35 +0700

    fix error status code

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,5 +1,5 @@
 import { HTTPException } from "hono/http-exception";
-import type { StatusCode } from "hono/utils/http-status";
+import type { ContentfulStatusCode as StatusCode } from "hono/utils/http-status";
 
 export function errorHttp(data: string, code: StatusCode = 500) {
   return new HTTPException(code, { message: data });
