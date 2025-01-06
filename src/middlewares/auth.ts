a1829939c64129eae98d3bd8fbc156d35adf3329
cecep <cecepjanuardi@outlook.com>
2025-01-06 22:15:59 +0700

    update auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -33,17 +33,18 @@ export const auth = createMiddleware(async (c, next) => {
 
     const decodedPayload = await verify(token, getSecret.jwt_secret);
     c.set("jwtPayload", decodedPayload);
+
     await next();
   } catch (error) {
-    if (error instanceof Error) {
-      return c.json(
-        {
-          message: "Unauthorized",
-          success: false,
-          requestId: c.get("requestId") || "N/A",
-        },
-        401
-      );
-    }
+    return c.json(
+      {
+        message: "Unauthorized",
+        success: false,
+        requestId: c.get("requestId") || "N/A",
+      },
+      401
+    );
+
+
   }
 });
