425d04ad507792f467e355f9eb240be4075aa2f8
cecep <cecepjanuardi@proton.me>
2025-09-19 08:41:49 +0700

    perf(database): increase max connection pool size from 30 to 50
    This change boosts the database connection pool capacity to handle higher loads and improve performance under increased traffic.

--- src/database/drizzel.ts
+++ src/database/drizzel.ts
@@ -6,7 +6,7 @@ import { getSecret } from "../config/secret";
 // Create the connection pool
 const connection = postgres(getSecret.db_url, {
   ssl: 'prefer',
-  max: 30,
+  max: 50,
   max_lifetime: 60 * 60, // 1 hour
   idle_timeout: 20 * 60, // 20 minutes
   connect_timeout: 10,
