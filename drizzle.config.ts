b9bb82dc2d18f28aafa43fc63218d39b5422119a
cecep <cecepjanuardi@proton.me>
2026-04-10 14:20:31 +0700

    feat(drizzle): add drizzle migrations config and migrate script
    enable drizzle migrations by configuring table and schema options
    add migrate script to package.json to run drizzle-kit migrate

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  dialect: "postgresql",
-  schema: "./src/database/schemas/postgres/schema.ts",
-  out: "./drizzle",
+  dialect: 'postgresql',
+  schema: './src/database/schemas/postgres/schema.ts',
+  out: './drizzle',
+  migrations: {
+    table: 'my-migrations-table', // `__drizzle_migrations` by default
+    schema: 'public', // used in PostgreSQL only, `drizzle` by default
+  },
   dbCredentials: {
-    url: process.env["DATABASE_URL"]!,
+    url: process.env['DATABASE_URL']!,
   },
 });
